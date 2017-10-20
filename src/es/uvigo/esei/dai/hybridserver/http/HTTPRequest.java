94a3242c05b21cd159e2fabfc6f9f5d5b2e334b0
Silvia Rodríguez <silviardguez@gmail.com>
2017-10-20 16:17:27 +0200

    Week 2

--- src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
+++ src/es/uvigo/esei/dai/hybridserver/http/HTTPRequest.java
@@ -4,237 +4,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.net.URLDecoder;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class HTTPRequest {
 
-	private HTTPRequestMethod metodo;
-	private String cadenaRecurso;
+	private BufferedReader reader;
+	private HTTPRequestMethod method;
+	private String resourceChain;
 	private String[] resourcePath;
-	private String resourceName;
 	private Map<String, String> resourceParameters;
-	private String httpVersion;
-	private Map<String, String> headersParameters;
+	private Map<String, String> resourceHeaderParameters;
+	private String resourceName;
+	private String version;
 	private String content;
 	private int contentLength;
 
-	// El constructor recibe un reader y tiene que parsear
 	public HTTPRequest(Reader reader) throws IOException, HTTPParseException {
-		try(BufferedReader buffer = new BufferedReader(reader))
-		{
-		inicializarValores(buffer);
-		}
+
+		this.reader = new BufferedReader(reader);
+
+		initValues();
 	}
 
 	public HTTPRequestMethod getMethod() {
-		return metodo;
+		return this.method;
 	}
 
 	public String getResourceChain() {
-		return cadenaRecurso;
+		return this.resourceChain;
 	}
 
 	public String[] getResourcePath() {
-		return resourcePath;
+		return this.resourcePath;
 	}
 
 	public String getResourceName() {
-		return resourceName;
+		return this.resourceName;
 	}
 
 	public Map<String, String> getResourceParameters() {
-		return resourceParameters;
+
+		return this.resourceParameters;
 	}
 
 	public String getHttpVersion() {
-		return httpVersion;
+		return this.version;
 	}
 
 	public Map<String, String> getHeaderParameters() {
-		return headersParameters;
+
+		return this.resourceHeaderParameters;
 	}
 
 	public String getContent() {
-		return content;
+		return this.content;
 	}
 
 	public int getContentLength() {
-
-		return contentLength;
-	}
-
-	public void setMetodo(String parametro) {
-		switch (parametro) {
-		case ("GET"):
-			this.metodo = HTTPRequestMethod.GET;
-			break;
-		case ("POST"):
-			this.metodo = HTTPRequestMethod.POST;
-			break;
-		case ("PUT"):
-			this.metodo = HTTPRequestMethod.PUT;
-			break;
-		case ("DELETE"):
-			this.metodo = HTTPRequestMethod.DELETE;
-			break;
-		case ("OPTIONS"):
-			this.metodo = HTTPRequestMethod.OPTIONS;
-			break;
-		case ("TRACE"):
-			this.metodo = HTTPRequestMethod.TRACE;
-			break;
-		case ("CONNECT"):
-			this.metodo = HTTPRequestMethod.CONNECT;
-			break;
-		case ("HEAD"):
-			this.metodo = HTTPRequestMethod.HEAD;
-			break;
-		}
-	}
-
-	private void inicializarValores(BufferedReader buffer) throws IOException, HTTPParseException {
-		String linea;
-
-		linea = buffer.readLine();
-		System.out.println(linea);
-
-		String[] division = linea.split(" ");
-
-		if (division.length != 3) {
-			throw new HTTPParseException("La primera linea no es correcta.");
-		}
-
-		switch (division[0]) {
-		case "GET":
-			setMetodo(division[0]);
-
-			System.out.println(division[1]);
-
-			this.cadenaRecurso = division[1];
-
-			resourceParameters = new LinkedHashMap<String, String>();
-			if (division[1].equals("/")) {
-				String[] path = new String[0];
-				this.resourcePath = path;
-			} else if (division[1].matches(".+\\?.+")) {
-				String[] allParam = division[1].split("\\?");
-				String[] consultas = allParam[1].split("&");
-
-				String aux = allParam[0].substring(1);
-				this.resourcePath = aux.split("/");
-
-				System.out.println("Esto es " + this.resourcePath.length);
-
-				for (int i = 0; i < consultas.length; i++) {
-					String[] divAux = consultas[i].split("=");
-					resourceParameters.put(divAux[0], divAux[1]);
-				}
-			} else if (division[1].substring(1).matches(".*/*")) {
-				String[] aux = division[1].substring(1).split("/");
-				this.resourcePath = aux;
-			}
-			/*
-			 * else if(division[1].substring(1)== ""){ String aux =
-			 * division[1].substring(1); this.resourcePath = aux.split("/");
-			 * System.out.println("Esto"+this.resourcePath.length); }
-			 */
-
-			String[] division1 = division[1].split("\\?");
-
-			this.resourceName = division1[0].substring(1);
-			this.httpVersion = division[2];
-
-			headersParameters = new LinkedHashMap<String, String>();
-
-			while ((linea = buffer.readLine()) != null && !linea.matches("")) {
-				if (linea.matches("Content-Length:.+")) {
-					String[] divisionAux = linea.split(": ");
-					this.contentLength = Integer.parseInt(divisionAux[1]);
-				}
-
-				else if (linea.matches(".*:.*")) {
-					String[] divCab = linea.split(": ");
-					headersParameters.put(divCab[0], divCab[1]);
-				} else {
-					throw new HTTPParseException("La cabecera no se ha escrito correctamente.");
-				}
-			}
-			break;
-		case "POST":
-			System.out.println("POST");
-			setMetodo(division[0]);
-			this.httpVersion = division[2];
-			
-			resourceParameters = new LinkedHashMap<String, String>();
-			this.cadenaRecurso = division[1];
-			this.resourceName = division[1].substring(1);
-			
-			if (division[1].equals("/")) {
-				String[] path = new String[0];
-				this.resourcePath = path;
-			}
-			else
-			{
-				this.resourcePath = division[1].substring(1).split("/");
-			}
-			
-			headersParameters = new LinkedHashMap<String, String>();
-			
-			while((linea = buffer.readLine())!=null && linea.matches(".*: .*"))
-			{
-				if (linea.matches("Content-Length:.+")) {
-					String[] divisionAux = linea.split(": ");
-					this.contentLength = Integer.parseInt(divisionAux[1]);
-					System.out.println("Content-Length: "+divisionAux[1]);
-				}			
-				    String[] parametros = linea.split(": ");
-				    headersParameters.put(parametros[0], parametros[1]);
-			}
-			
-			String contenido = buffer.readLine();
-			String type = headersParameters.get("Content-Type");
-			if (type != null && type.startsWith("application/x-www-form-urlencoded")) {
-			   this.content = URLDecoder.decode(contenido, "UTF-8");
-			}
-			else
-			{
-				this.content = contenido;
-			}
-			this.resourceParameters = new LinkedHashMap<String, String>();
-			if(this.content.matches(".*&.*"))
-			{
-				String[] aux1 = this.content.split("&");
-				
-				for(int i = 0;i<aux1.length;i++)
-				{
-					String[] aux2 = aux1[i].split("=");
-					this.resourceParameters.put(aux2[0], aux2[1]);
-					
-				}
-			}
-			else
-			{
-				String[] aux1 = this.content.split("=");
-				this.resourceParameters.put(aux1[0], aux1[1]);
-			}
-			break;
-		
-			/*if (this.getMethod().equals(HTTPRequestMethod.POST)) {
-				if (linea.matches(".*&.*")) {
-					resourceParameters = new LinkedHashMap<String, String>();
-					String[] cadenaAux = linea.split("&");
-					System.out.println(cadenaAux[1]);
-
-					for (int i = 0; i < cadenaAux.length; i++) {
-						String[] cadenaAux1 = cadenaAux[i].split("=");
-						resourceParameters.put(cadenaAux1[0], cadenaAux1[1]);
-					}
-				}
-
-			}*/
-		}
+		return this.contentLength;
 	}
 
 	@Override
@@ -251,5 +79,91 @@ public class HTTPRequest {
 		}
 
 		return sb.toString();
+	}
+
+	/**
+	 * Recupera información de petición HTTP
+	 * 
+	 * @throws IOException
+	 * @throws HTTPParseException si la petición HTTP es incorrecta.
+	 */
+	private void initValues() throws IOException, HTTPParseException {
+
+		// Dividir la primera línea de la petición para obtener el metodo, la
+		// cadena de recursos y la version
+		String[] firstLine = this.reader.readLine().split(" ");
+
+		if (firstLine.length != 3) {
+			throw new HTTPParseException("Invalid HTTPRequest first line.");
+		}
+
+		this.method = HTTPRequestMethod.valueOf(firstLine[0]);
+		this.resourceChain = firstLine[1];
+		this.version = firstLine[2];
+
+		this.resourceHeaderParameters = new LinkedHashMap<String, String>();
+		String line;
+		// Leer todas las líneas que contienen los parámetros de la cabecera
+		while ((line = this.reader.readLine()) != null && !line.matches("")) {
+
+			// Comprobar que están escritos en el formato correcto
+			if (line.matches(".*:.*")) {
+				String[] headerParameters = line.split(": ");
+				this.resourceHeaderParameters.put(headerParameters[0], headerParameters[1]);
+
+				// En el caso de que haya contenido se guardará su longitud
+				if (headerParameters[0].equals("Content-Length")) {
+					this.contentLength = Integer.parseInt(headerParameters[1]);
+				}
+
+			} else {
+				throw new HTTPParseException("La cabecera no se ha escrito correctamente.");
+			}
+		}
+
+		String[] parameters = null;
+		// Comprobar si los parámetros de la consulta forman parte del recurso solicitado
+		if (this.resourceChain.matches(".+\\?.+")) {
+			// Separar el nombre del recurso y los parámetros
+			String[] splitResource = this.resourceChain.split("\\?");
+			this.resourceName = splitResource[0].substring(1);
+			parameters = splitResource[1].split("\\&");
+
+		} else {
+			this.resourceName = this.resourceChain.substring(1);
+
+			// Comprobar si hay contenido
+			if (this.contentLength > 0) {
+				char[] contentArray = new char[this.contentLength];
+				if(this.reader.read(contentArray) != contentArray.length){
+					throw new HTTPParseException("Invalid content length");
+				}
+				
+				this.content = new String(contentArray);
+
+				// Descifrar el contenido
+				if (this.resourceHeaderParameters.get("Content-Type") != null && this.resourceHeaderParameters
+						.get("Content-Type").startsWith("application/x-www-form-urlencoded")) {
+					this.content = URLDecoder.decode(this.content, "UTF-8");
+				}
+				parameters = this.content.split("\\&");
+			}
+		}
+
+		this.resourceParameters = new LinkedHashMap<String, String>();
+		// Introducir en un mapa los parámetros de la consulta
+		if (parameters != null) {
+			String[] splitParameters;
+			for (int i = 0; i < parameters.length; i++) {
+				splitParameters = parameters[i].split("=");
+				this.resourceParameters.put(splitParameters[0], splitParameters[1]);
+			}
+		}
+
+		// Inicializar el resourcePath con longitud 0 por si es administrador
+		resourcePath = new String[0];
+		if (!resourceName.equals("")) {
+			resourcePath = this.resourceName.split("\\/");
+		}
 	}
 }
